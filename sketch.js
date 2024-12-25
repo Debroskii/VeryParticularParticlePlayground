b481bc0769aeaed0f529786bb34fdad728cd5495
E-DeBusk <debusk.elijah@gmail.com>
2024-12-25 13:15:50 -0800

    Added the testing attributes and emitter

--- sketch.js
+++ sketch.js
@@ -1,28 +1,26 @@
 const CANV_DIMENSIONS = [1495, 820]
 const FRAME_RATE = 60
 
+let attr
+
 function setup() {
   frameRate(FRAME_RATE)
   createCanvas(CANV_DIMENSIONS[0], CANV_DIMENSIONS[1]);
+  attr = new EmitterAttributes(10, 1, color(255, 255, 255))
+  attr.force_variation = 1
 }
 
 function draw() {
   background(0);
-  VeryParticularEngine.update()
-  VeryParticularEngine.draw()
+  print(frameRate())
+  VeryParticularEngine.tick()
 }
 
-function mouseDragged() {
-  VeryParticularEngine.particles.push(new Particle(
-    createVector(mouseX, mouseY),
-    createVector(0, 0),
-    1,
-    color(255, 255, 255)
-  ))
-}
-
-function out_of_bounds(position) {
-  if(position.x > CANV_DIMENSIONS[0] || position.y > CANV_DIMENSIONS[0]) return true;
-  else if(position.x < 0 || position.y < 0) return true;
-  else return false;
+function mouseClicked() {
+  VeryParticularEngine.sources.push(
+    new Emitter(
+      createVector(mouseX, mouseY),
+      attr
+    )
+  )
 }
