138ea5f4f0359a94175ddee9039a3fc7bf1985e2
E-DeBusk <debusk.elijah@gmail.com>
2024-12-25 10:41:31 -0800

    Commented some, and added a particle timeout variable

--- particular/global/config.js
+++ particular/global/config.js
@@ -1,4 +1,14 @@
 class Config {
+
+    // Gravity strength and direction for the engine
     static GRAVITY_MAGNITUDE = 9.8
-    static GRAVITY_ANGLE = PI
+    static GRAVITY_ANGLE = 0
+
+    // Returns a vector of gravity to directly apply to each particle
+    static getGravityVector() {
+        return createVector(this.GRAVITY_MAGNITUDE * sin(this.GRAVITY_ANGLE), this.GRAVITY_MAGNITUDE * cos(this.GRAVITY_ANGLE))
+    }
+
+    // How long the particle should be alive and ticking
+    static PARTICLE_TIMEOUT = 300
 }
