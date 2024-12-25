47d836176f2262e1b93a637f73ee18ee9c676bff
E-DeBusk <debusk.elijah@gmail.com>
2024-12-25 13:17:39 -0800

    Added color and size effects which govern how particle look as they travel or tick

---
+++ particular/components/attributes/size_effect.js
@@ -0,0 +1,8 @@
+class SizeEffect {
+    constructor(start_scale = 1, end_scale = 1, shrink = false, pulse = false) {
+        this.start_scale = start_scale
+        this.end_scale = end_scale
+        this.shrink = shrink
+        this.pulse = pulse
+    }
+}
