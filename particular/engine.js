ecbe211f5e60b5553093b3ce6364b4a1817926b3
E-DeBusk <debusk.elijah@gmail.com>
2024-12-25 10:40:38 -0800

    Added the base functionality of the Particle Engine

--- particular/engine.js
+++ particular/engine.js
@@ -1,3 +1,16 @@
 class VeryParticularEngine {
+    static particles = []
 
+    static update() {
+        for (const particle of VeryParticularEngine.particles) {
+            particle.applyForce(Config.getGravityVector())
+            particle.update()
+        }
+    }
+
+    static draw() {
+        for (const particle of VeryParticularEngine.particles) {
+            particle.draw()
+        }
+    }
 }
