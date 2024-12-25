59cd777ddd29b6b34ef42c950e30b4f3397ba5cb
E-DeBusk <debusk.elijah@gmail.com>
2024-12-25 13:18:29 -0800

    Moved out_of_bounds into util rather than the sketch file

--- particular/global/util.js
+++ particular/global/util.js
@@ -1,3 +1,7 @@
 class Util {
-    
+    static out_of_bounds(position) {
+        if(position.x > CANV_DIMENSIONS[0] || position.y > CANV_DIMENSIONS[1]) return true;
+        else if(position.x < 0 || position.y < 0) return true;
+        else return false;
+    }
 }
