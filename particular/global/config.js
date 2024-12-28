27437d005fbb65bf14da25343cafe0ffa8387586
E-DeBusk <debusk.elijah@gmail.com>
2024-12-27 21:46:15 -0800

    Moved the Environment's config into the registry system

--- particular/global/config.js
+++ particular/global/config.js
@@ -1,14 +1,30 @@
 class Config {
+    static registry
 
-    // Gravity strength and direction for the engine
-    static GRAVITY_MAGNITUDE = 0.0
-    static GRAVITY_ANGLE = 0
+    static init() {
+        Config.registry = new Registry("config");
+
+        // Gravity strength and direction for the engine
+        Config.registry.addNumber("gravity_mag", 0.0, "Gravity Magnitude")
+        Config.registry.addNumber("gravity_angle", 0.0, "Gravity Angle")
+
+        // Affector Gravitational constant
+        Config.registry.addNumber("gravitational_constant", 6.67, "Grav. Constant")
+
+        // How long the particle should be alive and ticking
+        Config.registry.addNumber("particle_timeout", 255, "Particle Timeout")
+        Config.registry.addNumber("launch_particle_timeout", 755, "Launched Particle Timeout")
+        Config.registry.addBoolean("launch_particle_trail", false, "Launched Particle Trail")
+
+        GlobalRegistry.addRegistry(Config.registry)
+    }
 
     // Returns a vector of gravity to directly apply to each particle
     static getGravityVector() {
-        return createVector(this.GRAVITY_MAGNITUDE * sin(this.GRAVITY_ANGLE), this.GRAVITY_MAGNITUDE * cos(this.GRAVITY_ANGLE))
+        return createVector(Config.registry.get("gravity_mag") * sin(Config.registry.get("gravity_angle") * PI/180), Config.registry.get("gravity_mag") * cos(Config.registry.get("gravity_angle") * PI/180))
     }
 
-    // How long the particle should be alive and ticking
-    static PARTICLE_TIMEOUT = 300
+    static getGravitationalForce(m1, m2, distance) {
+        return Config.registry.get("gravitational_constant") * ((m1 * m2) / (distance ** 2))
+    }
 }
