6e06d0503f8155ba8374e95167f9fba04652999c
E-DeBusk <debusk.elijah@gmail.com>
2024-12-27 21:44:56 -0800

    Added affector and launch_source support

--- particular/engine.js
+++ particular/engine.js
@@ -1,11 +1,38 @@
 class VeryParticularEngine {
     static sources = []
+    static launch_sources = []
+    static affectors = []
 
     static tick() {
-        for (const source of VeryParticularEngine.sources) {
+        console.log(VeryParticularEngine.launch_sources)
+
+        for(const source of VeryParticularEngine.sources) {
             source.spawn()
             source.update()
             source.draw()
+
+            for(const affector of VeryParticularEngine.affectors) {
+                affector.update(source.particles)
+            }
         }
+
+        for(const source of VeryParticularEngine.launch_sources) {
+            source.update()
+            source.draw()
+
+            for(const affector of VeryParticularEngine.affectors) {
+                affector.update(source.particles)
+            }
+        }
+
+        for(const affector of VeryParticularEngine.affectors) {
+            affector.draw()
+        }
+    }
+
+    static reset() {
+        if(!confirm("Are you sure you want to clear the engine?")) return
+        VeryParticularEngine.sources = []
+        VeryParticularEngine.affectors = []
     }
 }
