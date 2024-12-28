228843cf7d0f58a8684c23bf3a248491c1df03e9
E-DeBusk <debusk.elijah@gmail.com>
2024-12-27 21:44:22 -0800

    Changed CANV_DIMENSIONS to DIMENSIONS

--- particular/global/util.js
+++ particular/global/util.js
@@ -1,6 +1,6 @@
 class Util {
     static out_of_bounds(position) {
-        if(position.x > CANV_DIMENSIONS[0] || position.y > CANV_DIMENSIONS[1]) return true;
+        if(position.x > DIMENSIONS[0] || position.y > DIMENSIONS[1]) return true;
         else if(position.x < 0 || position.y < 0) return true;
         else return false;
     }
