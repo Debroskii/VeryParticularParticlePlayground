97a30bb4c10d9be7813ef86f46776ab15ec6a2dd
debroskii <debusk.elijah@gmail.com>
2025-01-10 22:14:54 -0800

    Update Affector text display and stroke color for better clarity

--- particular/components/affector.js
+++ particular/components/affector.js
@@ -31,15 +31,18 @@ class Affector extends Draggable {
             fill(255, 255)
             textFont("monospace")
             textSize(10)
-            text(this.registry.get("force").value < 0 ? `Attractor [${this.id}]` : this.registry.get("force").value > 0 ? `Repeller [${this.id}]` : `Affector [${this.id}]`, mouseX + 13, mouseY)
+            text((this.registry.get("force").value < 0 ? `Attractor [${this.id}]` : this.registry.get("force").value > 0 ? `Repeller [${this.id}]` : `UNSIGNED Affector [${this.id}]`), mouseX + 13, mouseY)
         }
 
+        strokeWeight(2)
         if(this.registry.get("force").value < 0) stroke(255, 205)
         else if(this.registry.get("force").value > 0) stroke(0, 205)
-        else stroke(255, 0, 255, 205)
+        else {
+            stroke(255, 0, 0, 255)
+            strokeWeight(2)
+        }
 
         fill(this.registry.get("force").value > 0 ? 255 : 0)
-        strokeWeight(2)
             if(this.draggable()) {
                 strokeWeight(3)
             }
